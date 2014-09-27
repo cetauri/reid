ea1d9f7c6ad3bddd56229d2f0b20d07eb07a8edb
Kwon, oh-sang <ohsang.kwon@kt.com>
2014-09-27 18:43:38 +0900

    add redis.io

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 "use strict";
 
 var os = require('os');
+var util = require("util");
+var redis = require("redis");
+var rClient = redis.createClient();//    rc = new redis.createClient(s.REDIS_PORT, s.REDIS_URL, null);
+
 var express = require('express');
 var app = express();
 app.listen(3000);
 
 app.get('/', function(req, res){
-	throw new Error("Can't divide by zero")
   res.send('Hello, I`m Dr. Spencer Reid');
 });
 
 app.get('/__info', function (req, res) {
-
     var _package = require('./package.json');
     res.json({
         hostname : os.hostname(),
@@ -19,24 +21,62 @@ app.get('/__info', function (req, res) {
         uptime: process.uptime(),
         memory: process.memoryUsage(),
         package: _package,
-        env: app.get('env')
+        env: app.get('env'),
+        redis : rClient.server_info.redis_version
     });
 });
 
 app.route('/cache')
   .get(function(req, res) {
-  	res.json({Method : req.method});
+
+  	rClient.get(req.query.key, function (err, reply) {
+  		if (err != null) {
+			res.json({value : "", message : err}); 	//TODO : REFACTOR
+			return;
+  		} 
+        res.json({value : reply});	
+    });
   })
   .post(function(req, res) {
-    res.json({Method : req.method});
+  	
+  	var key = req.query.key;
+  	var value = req.query.value;
+  	var ttl = req.query.ttl;
+
+    rClient.set(key, value);
+	rClient.expire(key, ttl);
+    res.json({status : 0});
   })
   .delete(function(req, res) {
-    res.json({Method : req.method});
+  	//TODO Exception
+  	var key = req.query.key;
+	rClient.del(key);	
+    res.json({status : 0});
+
   });
 
 app.post('/cache/new', function (req, res) {
-  	res.json({Method : req.method});
-  });
+	res.json({Method : req.method});
+});
+
+
+rClient.on("error", function (err) {
+    console.log("redis client error " + err);
+    throw err;
+});
+
+rClient.on('ready', function () {
+    console.log("redis client ready!");
+});
+
+
+var redisClient = redis.createClient();
+redisClient.monitor(function (err, res) {
+    console.log("Entering monitoring mode." + res);
+});
+redisClient.on("monitor", function (time, args) {
+    console.log(time + ": " + util.inspect(args));
+});
 
 
 process.on('uncaughtException', function (err) {
