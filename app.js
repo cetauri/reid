e6c76e07536123b3b5ec4464281224b97e708709
Kwon, oh-sang <ohsang.kwon@kt.com>
2014-09-30 16:10:15 +0900

    - error handling - MGET

--- app.js
+++ app.js
@@ -3,12 +3,26 @@
 var os = require('os');
 var util = require("util");
 var redis = require("redis");
-// var config = require("./config");
-var rClient = redis.createClient();//    rc = new redis.createClient(s.REDIS_PORT, s.REDIS_URL, null);
-
+var config = require("./config");
+var rClient = redis.createClient();
 var express = require('express');
+var errorhandler = require('errorhandler')
+
 var app = express();
 app.listen(3000);
+
+if (process.env.NODE_ENV === 'development') {
+  // only use in development
+  app.use(errorhandler())
+ 
+  var redisClient = redis.createClient();
+  redisClient.monitor(function (err, res) {
+    console.log("Entering monitoring mode." + res);
+  });
+  redisClient.on("monitor", function (time, args) {
+    console.log(time + ": " + util.inspect(args));
+  });
+} 
 
 app.get('/', function(req, res){
   res.send('Hello, I`m Dr. Spencer Reid');
@@ -17,13 +31,13 @@ app.get('/', function(req, res){
 app.get('/__info', function (req, res) {
     var _package = require('./package.json');
     res.json({
-        hostname : os.hostname(),
-        pid: process.pid,
-        uptime: process.uptime(),
-        memory: process.memoryUsage(),
-        package: _package,
-        env: app.get('env'),
-        redis : rClient.server_info.redis_version
+      hostname : os.hostname(),
+      pid: process.pid,
+      uptime: process.uptime(),
+      memory: process.memoryUsage(),
+      package: _package,
+      env: app.get('env'),
+      redis : rClient.server_info.redis_version
     });
 });
 
@@ -31,58 +45,79 @@ app.route('/cache')
   .get(function(req, res) {
 
   	rClient.get(req.query.key, function (err, reply) {
-  		if (err != null) {
-			 res.json({value : "", message : err}); 	//TODO : REFACTOR
-			return;
-  		} 
-        res.json({status : 0, value : reply});	
+  		if (err != undefined) {
+        res.json({status : -1, message : err.toString()}); 	//TODO : REFACTOR
+  		} else {
+        res.json({status : 0, value : reply});  
+      }
     });
+
   })
+
   .post(function(req, res) {
   	
   	var key = req.query.key;
   	var value = req.query.value;
-  	var ttl = req.query.ttl;
-    
-    if (ttl == undefined){
-        ttl = "2592000";  //30일
-    }
+  	var ttl = req.query.ttl || config.ttl;
     
     rClient.SETEX(key, ttl, value);
-    res.json({status : 0});
+    responseFinish(res);
   })
-  .delete(function(req, res) {
-  	//TODO Exception
-  	var key = req.query.key;
-	 rClient.del(key);	
-    res.json({status : 0});
 
+  .delete(function(req, res) {
+    var key = req.query.key;
+    rClient.del(key);	
+    responseFinish(res);
   });
 
-app.post('/cache/new', function (req, res) {
-	res.json({Method : req.method});
+app.get('/caches', function (req, res) {
+
+  var keys = req.query.key;
+  var len = keys.length;
+
+  rClient.mget(req.query.key, function (err, reply) {
+      if (err != null) {
+        res.json({status : -1, message : err.toString()});   //TODO : REFACTOR
+      } else {
+        var data = [];
+        for (var i = 0; i < len; i++){
+          data.push( {key: keys[i], value: reply[i]});
+        }
+        res.json({status : 0, list : data});  
+      } 
+    });
 });
 
+var responseFinish = function(res){
+    if (redisConfig.lock) {
+      res.json({status : -1, message : redisConfig.message});
+    }else{
+      res.json({status : 0});
+    }
+}
 
-rClient.on("error", function (err) {
-    console.log("redis client error " + err);
-    throw err;
-});
+var redisConfig = {
+  lock : false,
+  message : ""
+} 
 
 rClient.on('ready', function () {
-    console.log("redis client ready!");
+  redisConfig.lock = false;
+  redisConfig.message = "";
+
+  console.log("redis client ready!");
 });
 
+rClient.on("error", function (err) {
+  redisConfig.lock = true;
+  redisConfig.message = err.toString();
 
-var redisClient = redis.createClient();
-redisClient.monitor(function (err, res) {
-    console.log("Entering monitoring mode." + res);
-});
-redisClient.on("monitor", function (time, args) {
-    console.log(time + ": " + util.inspect(args));
+  console.log("redis client error " + err);
+  throw err;
 });
 
 
 process.on('uncaughtException', function (err) {
     console.log('uncaughtException : ' + err);
 });
+
---
+++ config.js
@@ -0,0 +1,12 @@
+"use strict";
+var Config = {
+    ttl: 2592000,
+    mail : {
+	    mailgunKey : 'key-5km-szcqsn7xz9ki8u3vhwj3xh4-peg0',
+	    // mail_sender : 'cetauri@dreamwiz.com',
+	    mail_sender : 'cetauri@gmail.com'
+    }
+    
+};
+
+module.exports = Config;
